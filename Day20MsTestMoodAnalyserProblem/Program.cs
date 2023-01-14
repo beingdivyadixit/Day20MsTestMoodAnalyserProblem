9a48e0204fec8197a2336b8bd99bb13c10b3c732
Divya <beingdivyadixit@gmail.com>
2023-01-14 16:50:48 +0530

    Welcome program

---
+++ Day20MsTestMoodAnalyserProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MsTestMoodAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to  Day-20 MS-Test Mood Analyser Problem");
+            Console.ReadLine();
+        }
+    }
+}
